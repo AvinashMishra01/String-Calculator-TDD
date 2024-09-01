e785164d9a22dcb385e3778cdd5b4ae770e5ac15
Avinash Mishra <avinashmishralu@gmail.com>
2024-09-02 01:35:25 +0530

    Test case: Multiple negative no

--- src/app/calulator/calulator.component.spec.ts
+++ src/app/calulator/calulator.component.spec.ts
@@ -46,6 +46,11 @@ describe('add method', () => {
   it('should throw an error for negative numbers', () => {
     expect(() => component.add('//;\n-1;2;-3')).toThrow(new Error('Negative numbers not allowed: -1, -3'));
   });
+  
+  it('should handle multiple negative numbers', () => {
+    expect(() => component.add('//;\n1;-2;3;-4')).toThrow(new Error('Negative numbers not allowed: -2, -4'));
+  });
+
 
   
 })
