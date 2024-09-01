726071cc7eba3422991f22d3fbe461530d6c8c28
Avinash Mishra <avinashmishralu@gmail.com>
2024-09-01 16:52:25 +0530

    Screen For Calculatoin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalulatorComponent } from './calulator/calulator.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { CalulatorComponent } from './calulator/calulator.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
