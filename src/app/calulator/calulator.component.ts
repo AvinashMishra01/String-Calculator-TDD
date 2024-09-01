51f37c2b0b279415e28b1114b760b1cae4915df7
Avinash Mishra <avinashmishralu@gmail.com>
2024-09-01 15:51:39 +0530

    Calculator Component Created

---
+++ src/app/calulator/calulator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calulator',
+  templateUrl: './calulator.component.html',
+  styleUrls: ['./calulator.component.css']
+})
+export class CalulatorComponent {
+
+}
